78ebf4215425bff7f309f0d3783ed71413d90dcc
elizayounger <elizayounger@hotmail.co.uk>
2024-12-11 17:27:01 +0000

    implementing automation for the song list population

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,21 +2,29 @@ import React from "react";
 import "./SearchResults.css";
 import Song from "./Song.js";
 
-export default function SearchResults() {
-    return (<div className="searchResults">
-        <div className="resultsTabs">
-            <button className="tab tabResults selected">RESULTS</button>
-            <button className="tab tabPlaylists">PLAYLISTS</button>
-        </div>
-        <div className="songResults">
-            < Song />
-            < Song />
-            < Song />
-            < Song />
-            < Song />
-            < Song />
-            < Song />
-            < Song />
-        </div>
-    </div>);
+export default function SearchResults({ resultsList = [] }) {
+    const songList = resultsList.length ? (
+        resultsList.map((song, index) => (
+            <Song
+                key={index}
+                songName={song.songName}
+                artist={song.artist}
+            />
+        ))
+    ) : (
+        <p className="noResults">No results found</p>
+    );
+
+    return (
+        <section className="searchResults">
+            <div className="resultsTabs">
+                <button className="tab tabResults selected">SEARCH RESULTS</button>
+                {/* <button className="tab tabPlaylists">PLAYLISTS</button> */}
+            </div>
+
+            <div className="songResults">
+                <ul>{songList}</ul>
+            </div>
+        </section>
+    );
 }
